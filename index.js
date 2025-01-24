b8be10746ba54b0e06bd894640af835069ab73b2
Mughees Subtain <160583245+mugheessubtain@users.noreply.github.com>
2025-01-24 10:32:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import 'dotenv/config'
 import cors from 'cors'
 const app = express();
 app.use(express.json()); //poori app pe laga he
-app.use(cors("*"))
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 //connect to database
 
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB connected"))
